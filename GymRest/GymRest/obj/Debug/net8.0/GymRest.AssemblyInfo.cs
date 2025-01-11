b59dab98ab205fe41e205cd05d9aa144024d7ae8
Rik Decan <83696539+RikDecan@users.noreply.github.com>
2025-01-11 23:59:54 +0100

    eeee

--- GymRest/GymRest/obj/Debug/net8.0/GymRest.AssemblyInfo.cs
+++ GymRest/GymRest/obj/Debug/net8.0/GymRest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GymRest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd92134aaf540b1e8037b5dfc79a55bb5e0250e1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+400b7d608e1116c30aaac3a9579c2db0be7a82fb")]
 [assembly: System.Reflection.AssemblyProductAttribute("GymRest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GymRest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
